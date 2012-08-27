56f495f98d9d25cdf7b722b9621137d349d1f42a
sgur <sgurrr@gmail.com>
2012-08-27 18:53:04 +0900

    Update test script.

--- t/test.sh
+++ t/test.sh
@@ -1,2 +1,9 @@
+pwd=`pwd`
+
+if [ ${pwd##*/} == t ] ; then
+	echo "cd .."
+	cd ..
+fi
+
 ../../bundle/vim-vspec/bin/vspec ../../bundle/vim-vspec . t/up2date.vim
 ../../bundle/vim-vspec/bin/vspec ../../bundle/vim-vspec . t/line.vim
